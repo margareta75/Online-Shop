daf30f29e7f6895297fd408eae1638909f6dc916
Michaela Sydow <91529037+Sydow75@users.noreply.github.com>
2022-02-27 13:41:11 +0100

    testar

--- modules/display.js
+++ modules/display.js
@@ -1,6 +1,51 @@
+const newDiv = document.querySelector('#newContainer')
+
+
 // Function to display products in HTML
 const displayProducts = (arr) => {
-  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++); {
+
+    let img = arr[1].imgSrc;
+    let name = arr[1].name;
+    let price = arr[1].price;
+    let stock = arr[1].stockBalance;
+
+    let div = document.createElement('div');//skapa div där products skall synas
+
+
+    let imgNew = document.createElement('img')
+    let nameNew = document.createElement('p');
+    let priceNew = document.createElement('p')
+    let stockNew = document.createElement('p');
+
+
+    imgNew.src = img;
+    nameNew.innerText = name
+    priceNew.innerText = price + ' kr'
+    stockNew.innerText = 'Stock balance: ' + stock
+
+
+    div.className = 'item-containerNew'
+
+    div.appendChild(imgNew)
+    div.appendChild(nameNew)
+    div.appendChild(priceNew)
+    div.appendChild(stockNew)
+    newDiv.appendChild(div)
+  }
+
 };
 
+
+
+
 export { displayProducts };
+
+
+
+
+
+
+
+
