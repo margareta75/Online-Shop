6f3bcdbc041c05d1cbe4fba39718a71a779fddf6
Michaela Sydow <91529037+Sydow75@users.noreply.github.com>
2022-02-27 13:51:22 +0100

    update display
    Uppdaterar i main då jag ej får det att funka via mikiBranch

--- modules/display.js
+++ modules/display.js
@@ -4,3 +4,47 @@ const displayProducts = (arr) => {
 };
 
 export { displayProducts };
+
+const newDiv = document.querySelector('#newContainer')
+
+
+// Function to display products in HTML
+const displayProducts = (arr) => {
+
+  for (let i = 0; i < arr.length; i++); {
+
+    let img = arr[i].imgSrc;
+    let name = arr[i].name;
+    let price = arr[i].price;
+    let stock = arr[i].stockBalance;
+
+    let div = document.createElement('div');//skapa div där products skall synas
+
+
+    let imgNew = document.createElement('img')
+    let nameNew = document.createElement('p');
+    let priceNew = document.createElement('p')
+    let stockNew = document.createElement('p');
+
+
+    imgNew.src = img;
+    nameNew.innerText = name
+    priceNew.innerText = price + ' kr'
+    stockNew.innerText = 'Stock balance: ' + stock
+
+
+    div.className = 'item-containerNew'
+
+    div.appendChild(imgNew)
+    div.appendChild(nameNew)
+    div.appendChild(priceNew)
+    div.appendChild(stockNew)
+    newDiv.appendChild(div)
+  }
+
+};
+
+
+
+
+export { displayProducts };
